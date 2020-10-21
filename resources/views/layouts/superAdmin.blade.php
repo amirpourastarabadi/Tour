0efee3fbdf27677f6d0b2de69823aa5a3f697338
paradise <pormehriust@gmail.com>
2020-10-21 20:49:18 +0330

    create super admin layout #2

--- resources/views/layouts/superAdmin.blade.php
+++ resources/views/layouts/superAdmin.blade.php
@@ -1,108 +1,123 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <!-- Material Kit CSS -->
+    <link href="{{ asset('assets/css/material-dashboard.css?v=2.1.2') }}" rel="stylesheet" />
 </head>
+
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+<div class="wrapper ">
+    <div class="sidebar" data-color="purple" data-background-color="white">
+        <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+        <div class="logo">
+            <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+                CT
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+                Creative Tim
+            </a>
+        </div>
+        {{--    <main class="py-5">--}}
+        {{--        <div class="container d-flex">--}}
+        {{--            <div class="col-3">--}}
+        {{--                <div class="text-center"><h3></h3></div>--}}
+        {{--                <hr>--}}
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+        {{--                <div class="text-center">--}}
+        {{--                    <a href="" class="text-dark text-decoration-none">لیست ادمین ها</a>--}}
+        {{--                </div>--}}
 
-                </ul>
+        {{--                <div class="text-center">--}}
+        {{--                    <a href="" class="text-dark text-decoration-none">اضافه کردن ادمین</a>--}}
+        {{--                </div>--}}
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
+        {{--                <div class="text-center">--}}
+        {{--                    <a href="" class="text-dark text-decoration-none">ویرایش اطلاعات</a>--}}
+        {{--                </div>--}}
+
+        {{--                <div class="text-center">--}}
+        {{--                    <a href="" class="text-dark text-decoration-none">خروج</a>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
+
+
+        <div class="sidebar-wrapper">
+            <ul class="nav">
+                <li class="nav-item active  ">
+                    <a class="nav-link" href="#">
+                        <i class="material-icons">dashboard</i>
+                        <p>Passengers</p>
+                    </a>
+                </li>
+                <!-- your sidebar here -->
+            </ul>
+        </div>
+    </div>
+    <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+            <div class="container-fluid">
+                <div class="navbar-wrapper">
+                    <a class="navbar-brand" href="#">Super Admin Dashboard</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end">
+                    <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                            <a class="nav-link" href="javascript:;">
+                                <i class="material-icons">notifications</i> Notifications
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
                         </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <main class="py-5">
-        <div class="container d-flex">
-            <div class="col-3">
-                <div class="text-center"><h3>پنل سوپر ادمین</h3></div>
-                <hr>
-
-                <div class="text-center">
-                    <a href="" class="text-dark text-decoration-none">لیست ادمین ها</a>
-                </div>
-
-                <div class="text-center">
-                    <a href="" class="text-dark text-decoration-none">اضافه کردن ادمین</a>
-                </div>
-
-                <div class="text-center">
-                    <a href="" class="text-dark text-decoration-none">ویرایش اطلاعات</a>
-                </div>
-
-                <div class="text-center">
-                    <a href="" class="text-dark text-decoration-none">خروج</a>
+                        <!-- your navbar here -->
+                    </ul>
                 </div>
             </div>
-
-
-
-
-            <div class="container ml-2 col-8">@yield('content')</div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="content">
+            <div class="container-fluid">
+                <!-- your content here -->
+                @yield('content')
+            </div>
         </div>
-
-    </main>
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="float-left">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Tour
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- your footer here -->
+            </div>
+        </footer>
+    </div>
 </div>
+@stack('js')
 </body>
-</html>
 
+</html>
