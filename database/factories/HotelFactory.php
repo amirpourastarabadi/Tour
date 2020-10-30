25fe66078382fc81a587a5d2e3bd694e6a660662
amir pouresterabadi <amirpourastarabadi@gmail.com>
2020-10-30 16:07:19 +0330

    add hotel factory

---
+++ database/factories/HotelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Hotel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HotelFactory extends Factory
+{
+
+    protected $model = Hotel::class;
+
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'address' => $this->faker->address,
+            'phone_number' => $this->faker->unique()->e164PhoneNumber,
+            'stars' => rand(1, 5),
+            'image' => $this->faker->imageUrl()
+        ];
+    }
+}
--- database/seeders/HotelSeeder.php
+++ database/seeders/HotelSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hotel;
 use Illuminate\Database\Seeder;
 
 class HotelSeeder extends Seeder
 {
     public function run()
     {
-        //
+        Hotel::factory(10);
     }
 }
