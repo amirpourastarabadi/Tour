1b195671b24a34ff904f43a0dabd591a59e0663c
paradise <pormehriust@gmail.com>
2020-11-15 18:52:27 +0330

    fix old value in completion tourAdmin form

--- resources/views/passenger/registerCompletion.blade.php
+++ resources/views/passenger/registerCompletion.blade.php
@@ -34,7 +34,7 @@
 
             <div class="form-group col-md-6">
                 <label>{{ __('passenger.register_completion.email') }}</label>
-                <input name="email" type="email" placeholder="{{ __('passenger.register_completion.email') }}" class="form-control col-8 @if($errors->passengerCompletion->first('email')) is-invalid @endif">
+                <input name="email" type="email" value="{{ old('email') }}" placeholder="{{ __('passenger.register_completion.email') }}" class="form-control col-8 @if($errors->passengerCompletion->first('email')) is-invalid @endif">
                 @if($errors->passengerCompletion->first('email'))
                     <span class="container text-danger text-small" role="alert">
                         <strong>{{ $errors->passengerCompletion->first('email') }}</strong>
@@ -44,7 +44,7 @@
 
             <div class="form-group bmd-form-group col-md-6">
                 <label>{{ __('passenger.register_completion.telephone_number') }}</label>
-                <input name="telephone_number" type="text" placeholder="{{ __('passenger.register_completion.telephone_number_description') }}" class="form-control col-8 @if($errors->passengerCompletion->first('telephone_number')) is-invalid @endif">
+                <input name="telephone_number" value="{{ old('telephone_number') }}" type="text" placeholder="{{ __('passenger.register_completion.telephone_number_description') }}" class="form-control col-8 @if($errors->passengerCompletion->first('telephone_number')) is-invalid @endif">
 
                 @if($errors->passengerCompletion->first('telephone_number'))
                     <span class="container text-danger text-small" role="alert">
@@ -59,7 +59,7 @@
         <div class="form-row">
             <div class="form-group bmd-form-group col-md-6">
                 <label>{{ __('passenger.register_completion.national_code') }}</label>
-                <input name="national_code" type="text" placeholder="{{ __('passenger.register_completion.national_code') }}" class="form-control col-8 @if($errors->passengerCompletion->first('national_code')) is-invalid @endif">
+                <input name="national_code" value="{{ old('national_code') }}" type="text" placeholder="{{ __('passenger.register_completion.national_code') }}" class="form-control col-8 @if($errors->passengerCompletion->first('national_code')) is-invalid @endif">
 
                 @if($errors->passengerCompletion->first('national_code'))
                     <span class="container text-danger text-small" role="alert">
@@ -70,7 +70,7 @@
 
             <div class="form-group col-md-6">
                 <label>{{ __('passenger.register_completion.birthday') }}</label>
-                <input name="birthday" type="date" class="form-control col-8 @if($errors->passengerCompletion->first('birthday')) is-invalid @endif">
+                <input name="birthday" value="{{ old('birthday') }}" type="date" class="form-control col-8 @if($errors->passengerCompletion->first('birthday')) is-invalid @endif">
                 @if($errors->passengerCompletion->first('birthday'))
                     <span class="container text-danger text-small" role="alert">
                         <strong>{{ $errors->passengerCompletion->first('birthday') }}</strong>
